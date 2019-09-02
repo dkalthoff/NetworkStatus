0dfa6d056cdc06867f8d0420c235ccfa3a74a9cb
Dave Kalthoff <dkalthoff@hotmail.com>
2019-09-02 18:13:08 -0500

    Add DisplayService and NetworkService to connect to WiFi

--- NetworkStatus.ino
+++ NetworkStatus.ino
@@ -1,14 +1,23 @@
-#include <LiquidCrystal.h>
 
-const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2
-LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
+
+
+
+
+
 
 void setup() {
-  lcd.begin(16, 2);
-
+    // Open serial communications and wait for port to open:
+  Serial.begin(9600);
+  while (!Serial) {
+    ; // wait for serial port to connect. Needed for native USB port only
+  }
+  
+  initDisplay();
+  connectToWiFi();
+  sendToDisplay("Connected to WiFi", 1);
 }
 
 void loop() {
-  lcd.print("hello, world!");
-
+  
+  delay(1000); 
 }
